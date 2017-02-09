89be0c31ce88e79885536f93aae52584d7d3b101
Tommy-Lim <tlim24@gmail.com>
2017-02-09 10:18:12 -0800

    completed match brackets

--- bracket-matching.py
+++ bracket-matching.py
@@ -16,9 +16,59 @@ class Stack:
     def __len__(self):
         return self.size()
 
-ss = Stack()
+    def __str__(self):
+        return "{}".format(self.data)
 
-ss.push(42)
-ss.push(10)
-ss.push(9)
-print(len(ss))
+def test_brackets(string):
+    ss = Stack()
+    for char in string:
+        if char == "{" or char == "(" or char == "[":
+            ss.push(char)
+            print(ss)
+        elif char =="}" and ss.pop() == "{":
+            print(ss)
+        elif char ==")" and ss.pop() == "(":
+            print(ss)
+        elif char =="]" and ss.pop() == "[":
+            print(ss)
+        elif char == "}" or char == ")" or char == "]":
+            print(False)
+            return False
+    if len(ss) == 0:
+        print(True)
+        return True
+    else:
+        print(False)
+        return False
+
+# True
+test_brackets('abc(123)')
+print()
+
+#returns True
+test_brackets('a[bc(123)]')
+print()
+
+#returns True
+test_brackets('a{b}{c(1[2]3)}')
+print()
+
+#returns True
+test_brackets('()')
+print()
+
+#returns True -- no brackets = correctly matched
+test_brackets('abc123yay')
+print()
+
+#returns False -- missing closing bracket
+test_brackets('abc(123')
+print()
+
+#returns False -- inproperly nested
+test_brackets('a[bc(12]3)')
+print()
+
+#returns False -- inproperly nested
+test_brackets('a{b}{c(1}[2]3)')
+print()
